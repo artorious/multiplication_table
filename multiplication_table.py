d053917fd3941bdb197d19a668752641ab48686f
Arthur Ngondo <ngondoarthur@gmail.com>
2017-12-13 19:56:30 +0300

    Add row and column header

--- multiplication_table.py
+++ multiplication_table.py
@@ -7,8 +7,22 @@ except ValueError:
     print('Oops!.... Expected Integer')
 
 # Print a size x size multiplcation table
+
+# Headers  
+print('     ', end='') # Blank top-leftmost cell
+for column in range(1, size + 1):
+    print('{0:4}'.format(column), end='') # Column Header Number
+print() # Move down to the next line
+# col-header and rows sepaartor
+print('    +', end='')
+for column in range(1, size + 1):
+    print('----', end='') # Separator line
+print() # Move down to next line
+
+# Rows and table content
 for row in range(1, size + 1):
+    print('{0:3} |'.format(row), end='')
     for column in range(1, size + 1):
         product = row * column # Compute product
-        print('{0:4}'.format(product), end=' ')
+        print('{0:4}'.format(product), end='')
     print() # Move cursor to the next row
